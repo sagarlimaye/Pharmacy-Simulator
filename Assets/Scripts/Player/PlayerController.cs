0bd2fc24808599b4b708dc7df3706eec1cdeb3eb
Sagar Limaye <sagarl3232@hotmail.com>
2018-02-25 13:58:56 -0600

    Added files

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float walkSpeed;
+	public float gravity;
+
+	CharacterController controller;
+	Vector3 inputDirection = Vector3.zero;
+	Vector3 horizontal = Vector3.zero;
+	Vector3 vertical = Vector3.zero;
+	Vector3 moveDirection = Vector3.zero;
+	float verticalVelocity;
+	float verticalVelocityMax = 50.0f;
+	float verticalVelocityMin = -50.0f;
+
+	void Awake() {
+		controller = GetComponent<CharacterController>();
+	}
+
+	void Update() {
+		// Update is for non-physics-related code
+
+		// Get player inputs for x and z axis
+		inputDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+	}
+
+	void FixedUpdate() {
+		// Update is for physics-related code
+		MovePlayer();
+		ApplyGravity();
+	}
+
+	void MovePlayer() {
+		// Split horizontal (x, z) and vertical (y) values to normalize diagonal movement without changing aerial physics
+		Vector3 transformDirection = transform.TransformDirection(inputDirection);
+		horizontal = new Vector3(transformDirection.x, 0.0f, transformDirection.z);
+		horizontal = Vector3.ClampMagnitude(horizontal, 1);
+		vertical = new Vector3(0.0f, verticalVelocity, 0.0f);
+
+		// Apply walk speed to normalized horizontal axis
+		horizontal.x *= walkSpeed;
+		horizontal.z *= walkSpeed;
+
+		// Combine normalized horizontal movement with vertical movement before moving
+		moveDirection = horizontal + vertical;
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+
+	void ApplyGravity() {
+		if (controller.isGrounded == true) {
+			verticalVelocity = -1;
+		} else {
+			verticalVelocity -= gravity * Time.deltaTime;
+			verticalVelocity = Mathf.Clamp(verticalVelocity, verticalVelocityMin, verticalVelocityMax);
+		}
+	}
+}
---
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour {
+
+	public Transform playerBody;
+	public float mouseSensitivity;
+
+	float xAxisClamp = 0.0f;
+
+	void Update() {
+		Cursor.lockState = CursorLockMode.Locked;
+
+		if (Cursor.lockState == CursorLockMode.Locked) {
+			RotateCamera();
+		}
+	}
+
+	void RotateCamera() {
+		float mouseX = Input.GetAxis("Mouse X");
+		float mouseY = Input.GetAxis("Mouse Y");
+
+		float rotateAmountX = mouseX * mouseSensitivity;
+		float rotateAmountY = mouseY * mouseSensitivity;
+
+		xAxisClamp -= rotateAmountY;
+
+		Vector3 targetRotationCamera = transform.rotation.eulerAngles;
+		Vector3 targetRotationBody = playerBody.rotation.eulerAngles;
+
+		targetRotationCamera.x -= rotateAmountY;
+		targetRotationCamera.z = 0;
+		targetRotationBody.y += rotateAmountX;
+
+		if (xAxisClamp > 90) {
+			xAxisClamp = 90;
+			targetRotationCamera.x = 90;
+		} else if (xAxisClamp < -90) {
+			xAxisClamp = -90;
+			targetRotationCamera.x = 270;
+		}
+
+		transform.rotation = Quaternion.Euler(targetRotationCamera);
+		playerBody.rotation = Quaternion.Euler(targetRotationBody);
+	}
+}
