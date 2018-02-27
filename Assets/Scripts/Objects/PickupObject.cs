a7b098846354822bf9a99e8191bf31e3ad639223
jmholt9 <jmholt9@users.noreply.github.com>
2018-02-27 21:30:57 +0000

    Added PickupObject.cs, allows player to grab and pick up things

---
+++ Assets/Scripts/Objects/PickupObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupObject : MonoBehaviour {
+	public Transform isHeld;
+	private float distance;
+
+	//Called once per frame
+	void Update(){
+		distance = Vector3.Distance(transform.position, GameObject.Find("Player").transform.position);
+	}
+
+	//When player clicks and holds mouse, they pick up the object
+	void OnMouseDown(){
+		//Player must be within a minimum range of the object in order to successfully grab it
+		if(distance < 3){
+			GetComponent<Rigidbody>().useGravity  = false;
+			GetComponent<Rigidbody>().isKinematic = true;
+			transform.position = isHeld.position;
+			transform.parent   = GameObject.Find("Player").transform;
+			transform.parent   = GameObject.Find("FirstPersonPlayer").transform;
+		}
+	}
+
+	//When player lets go of mouse, they're no longer holding the object
+	void OnMouseUp(){
+		transform.parent = null;
+		GetComponent<Rigidbody>().useGravity  = true;
+		GetComponent<Rigidbody>().isKinematic = false;
+	}
+}
