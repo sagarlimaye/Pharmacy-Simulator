ca15598e23331b1d8c7d72bf130c25f4ade721a6
jmholt9 <jmholt9@users.noreply.github.com>
2018-03-06 00:03:46 -0600

    Added MonitorInteraction.cs

---
+++ Assets/Scripts/Objects/MonitorInteraction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MonitorInteraction : MonoBehaviour {
+	public Text notification;
+
+	private float distance;
+	
+	// Update is called once per frame
+	void Update () {
+		distance = Vector3.Distance(transform.position, GameObject.Find("Player").transform.position);
+
+		notification.text = distance < 3 ? "Press E to use." : "";
+	}
+}
