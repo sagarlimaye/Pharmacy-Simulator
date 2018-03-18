28290664bd0910886c670aa18733c0778ba92545
Sagar Limaye <sagarl3232@hotmail.com>
2018-03-18 15:12:06 -0500

    Updated player follower to only follow player if within certain range

--- Assets/PlayerFollower.cs
+++ Assets/PlayerFollower.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class PlayerFollower : MonoBehaviour {
 
     public PlayerController player;
+    public float maxRange=10f;
+
     private float speed = 2;
 	
 	// Update is called once per frame
 	void Update () {
         var step = speed * Time.deltaTime;
-        if(Vector3.Distance(transform.position, player.transform.position) > 2)
+        var distance = Vector3.Distance(transform.position, player.transform.position); 
+        if(distance > 2 && distance<=maxRange)
             transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
 	}
 }
