ba3eef71ce43411e4a33480795ddec0e53c5bcd4
Sagar Limaye <sagarl3232@hotmail.com>
2018-03-31 12:05:47 -0500

    Added Navigation mesh
    Also updated player following script to update position using NavMesh agent

--- Assets/PlayerFollower.cs
+++ Assets/PlayerFollower.cs
@@ -1,19 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.AI;
 using UnityEngine;
 
 public class PlayerFollower : MonoBehaviour {
 
-    public PlayerController player;
-    public float maxRange=10f;
+    public Transform target;
+    NavMeshAgent agent;
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = target.position;
+    }
 
-    private float speed = 2;
-	
 	// Update is called once per frame
 	void Update () {
-        var step = speed * Time.deltaTime;
-        var distance = Vector3.Distance(transform.position, player.transform.position); 
-        if(distance > 2 && distance<=maxRange)
-            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
+       
 	}
 }
