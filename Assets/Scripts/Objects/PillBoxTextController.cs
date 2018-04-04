96384e96a19c3ddb8a035ab656c9d0846480bc3c
huedev <32443158+huedev@users.noreply.github.com>
2018-04-04 09:50:23 -0500

    Prescription Label with info text, Pill Bottle, Pill Box with drug text

---
+++ Assets/Scripts/Objects/PillBoxTextController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillBoxTextController : MonoBehaviour {
+
+	public string drug;
+
+	TextMesh textMesh;
+
+	// Use this for initialization
+	void Start () {
+		textMesh = GetComponent<TextMesh>();
+		textMesh.text = drug;
+	}
+}
---
+++ Assets/Scripts/Objects/PrescriptionTextController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrescriptionTextController : MonoBehaviour {
+
+	public string customerFirstName;
+	public string customerLastName;
+	public string doctorFirstName;
+	public string doctorLastName;
+	public string drug;
+	public string instructions;
+	public string quantity;
+	TextMesh textMesh;
+
+	// Use this for initialization
+	void Start () {
+		textMesh = GetComponent<TextMesh>();
+		textMesh.text = customerLastName + ", " + customerFirstName + "\n" + "Dr. " + doctorLastName + ", " + doctorFirstName + "\n" + drug + "\n" + instructions + "\n" + quantity;
+	}
+}
