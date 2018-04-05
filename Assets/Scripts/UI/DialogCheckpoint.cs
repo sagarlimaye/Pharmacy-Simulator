006b91734e328cca8bee75797bbaa0bb552a2fce
Sagar Limaye <sagarl3232@hotmail.com>
2018-04-05 14:19:50 -0500

    Trigger dialog only if both customer and player are inside trigger

--- Assets/Scripts/UI/DialogCheckpoint.cs
+++ Assets/Scripts/UI/DialogCheckpoint.cs
@@ -6,13 +6,37 @@ public class DialogCheckpoint : MonoBehaviour {
 
     public DialogueController controller;
     //public Dialog[] sentences;
+    BoxCollider boxCollider;
 
+    public GameObject dialog;
+    public bool isPlayer, isCustomer;
+    
+    void Start()
     {
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {    
+        if(other.tag == "Player")
+            isPlayer = true;
+        if(other.tag == "Customer")
+            isCustomer = true;
+        if(isPlayer && isCustomer)
+            if(!controller.busy && dialog != null)
                 activate();
-            
-        }
+                
+        // if(player not busy)
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+            isPlayer = false;
+        if(other.tag == "Customer")
+            isCustomer = false;
     }
     public void activate()
     {
+        controller.startDialog(dialog);        
     }
 }
