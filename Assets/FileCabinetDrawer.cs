046ea28f447a4b57b33070e71cd7353644fd4af7
Sagar Limaye <sagarl3232@hotmail.com>
2018-04-18 23:42:00 -0500

    Added mechanic to open file cabinet drawers

---
+++ Assets/FileCabinetDrawer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FileCabinetDrawer : MonoBehaviour {
+    private float distance;
+    private bool closed = true;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        distance = Vector3.Distance(transform.position, GameObject.Find("Player").transform.position);
+
+        if (distance < 3 && Input.GetKeyDown(KeyCode.E) && PlayerLook.hitObject == gameObject)
+        {
+            if (closed)
+            {
+                transform.position += new Vector3(0, 0, 1);
+                closed = false;
+            }
+            else
+            {
+                closed = true;
+                transform.position += new Vector3(0, 0, -1);
+            }
+        }
+    }
+}
